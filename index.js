e3eff0f5d9fcba1e58fce8d5062db46c8490aded
Vovan41k <fatuev_vladimir@mail.ru>
2022-01-14 20:45:06 +0300

    task 1.1 completed

---
+++ index.js
@@ -0,0 +1,7 @@
+const NumberArray = require ("./lib/NumberArray")
+const numbers = [2,3,5,7,9,11,13,-15,-17]
+console.log(NumberArray.max(numbers))
+console.log(NumberArray.min(numbers))
+console.log(NumberArray.avg(numbers))
+
+
