5c9ad573f747b8daf6a99b084f4545d2cc7fe665
Vovan41k <fatuev_vladimir@mail.ru>
2022-01-21 21:03:12 +0300

    tusk 1 - count

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const NumberArray = require ("./lib/NumberArray")
-const numbers = [2,3,5,7,9,11,13,-15,-17]
+const numbers = [2,3,5,7,9,11,13,-15,-17,2,3]
 console.log(NumberArray.max(numbers))
 console.log(NumberArray.min(numbers))
 console.log(NumberArray.avg(numbers))
+console.log(NumberArray.count(numbers, 5))
+console.log(NumberArray.filter(numbers, "even"))
+console.log(NumberArray.filter(numbers, "odd"))
+console.log(NumberArray.filter(numbers, "div", 3))
 
 
